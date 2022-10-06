581e4aa176228ecd8960feccea1a6f3ef680d932
Navarette <navarette.arnold@iisgalvanimi.edu.it>
2022-10-06 10:01:05 +0000

    punto 2

--- Prep_ver_02.2/src/app/container/container.component.ts
+++ Prep_ver_02.2/src/app/container/container.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
-  constructor() { }
-
+  m_name: string[];
+  constructor() { 
+    this.m_name = ['ruspa','escavatore','Bulldozer','betoniera','dumper','gru','rullo compattatore','minipale','Carrello elevatore','trattore']
+  }
+    
   ngOnInit(): void {
   }
 
